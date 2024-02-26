c798f584468b2d3da8d914d74f3fced3aa4a6dfb
YatishPatil1234 <yatish.203869112@vcet.edu.in>
2024-02-26 14:08:58 +0530

    Script.js file updated

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+function add(a) {
+    return function (b) {
+        return function (c) {
+            return a + b + c;
+        }
+       
+    }
+}
+
+let result = add(2)(3)(4);
+result;
